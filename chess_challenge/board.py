8b204bb9c0cab928fdaf2cd3d34f3bf4f7426a34
Bartolomé Sánchez <bsanchezsalado@gmail.com>
2016-04-19 16:54:27 +0200

    Change iteration order (rows x cols) for getting coordinates

--- chess_challenge/board.py
+++ chess_challenge/board.py
@@ -5,6 +5,6 @@ Board related functions.
 
 def get_coordinates_from_board(rows, cols):
     """Returns an generator with all coordinates from a board."""
-    for i in range(rows):
-        for j in range(cols):
+    for j in range(cols):
+        for i in range(rows):
             yield (i, j)
--- tests/test_board.py
+++ tests/test_board.py
@@ -6,13 +6,13 @@ class TestBoard:
         assert list(board.get_coordinates_from_board(1, 1)) == [(0, 0)]
 
     def test_get_coordinates_from_board__2x2(self):
-        result = [(0, 0), (0, 1), (1, 0), (1, 1)]
+        result = [(0, 0), (1, 0), (0, 1), (1, 1)]
         assert list(board.get_coordinates_from_board(2, 2)) == result
 
     def test_get_coordinates_from_board__3x2(self):
-        result = [(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1)]
+        result = [(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1)]
         assert list(board.get_coordinates_from_board(3, 2)) == result
 
     def test_get_coordinates_from_board__2x3(self):
-        result = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+        result = [(0, 0), (1, 0), (0, 1), (1, 1), (0, 2), (1, 2)]
         assert list(board.get_coordinates_from_board(2, 3)) == result
