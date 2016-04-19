c7039284a0f11c79a7643b132bb7ccdc1f0ddb97
Bartolomé Sánchez <bsanchezsalado@gmail.com>
2016-04-19 18:00:25 +0200

    Add root menaces detection function

--- chess_challenge/pieces.py
+++ chess_challenge/pieces.py
@@ -7,11 +7,12 @@ def get_menaced_positions(piece, position, rows, cols):
     """Determine the function to call for a given piece."""
     PIECES_FUNCTIONS = {
         'K': get_menaced_king_positions,
+        'R': get_menaced_rook_positions,
     }
-    return PIECES_FUNCTIONS[piece](piece, position, rows, cols)
+    return PIECES_FUNCTIONS[piece](position, rows, cols)
 
 
-def get_menaced_king_positions(piece, position, rows, cols):
+def get_menaced_king_positions(position, rows, cols):
     """Return all menaced position by a king."""
     result = []
 
@@ -28,5 +29,34 @@ def get_menaced_king_positions(piece, position, rows, cols):
 
     if current_j - 1 >= 0:
         result.append((current_i, current_j - 1))
+
+    return result
+
+
+def get_menaced_rook_positions(position, rows, cols):
+    """Return all menaced positions by a rook."""
+    result = []
+
+    current_i, current_j = position
+
+    i = current_i
+    while i + 1 < rows:
+        i += 1
+        result.append((i, current_j))
+
+    i = current_i
+    while i - 1 >= 0:
+        i -= 1
+        result.append((i, current_j))
+
+    j = current_j
+    while j + 1 < cols:
+        j += 1
+        result.append((current_i, j))
+
+    j = current_j
+    while j - 1 >= 0:
+        j -= 1
+        result.append((current_i, j))
 
     return result
--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -7,18 +7,37 @@ class TestMenacedPositionsCalls:
     @mock.patch('chess_challenge.pieces.get_menaced_king_positions')
     def test_calling_king(self, king_mock):
         pieces.get_menaced_positions('K', (0, 0), 3, 3)
-        king_mock.assert_called_once_with('K', (0, 0), 3, 3)
+        king_mock.assert_called_once_with((0, 0), 3, 3)
+
+    @mock.patch('chess_challenge.pieces.get_menaced_rook_positions')
+    def test_calling_rook(self, rook_mock):
+        pieces.get_menaced_positions('R', (0, 0), 3, 3)
+        rook_mock.assert_called_once_with((0, 0), 3, 3)
 
 
 class TestKing:
     def test_menaced_king_positions__left_top_corner(self):
         result = [(1, 0), (0, 1)]
-        assert pieces.get_menaced_king_positions('K', (0, 0), 3, 3) == result
+        assert pieces.get_menaced_king_positions((0, 0), 3, 3) == result
 
     def test_menaced_king_positions__center(self):
         result = [(2, 1), (0, 1), (1, 2), (1, 0)]
-        assert pieces.get_menaced_king_positions('K', (1, 1), 3, 3) == result
+        assert pieces.get_menaced_king_positions((1, 1), 3, 3) == result
 
     def test_menaced_king_positions__right_bottom_corner(self):
         result = [(1, 2), (2, 1)]
-        assert pieces.get_menaced_king_positions('K', (2, 2), 3, 3) == result
+        assert pieces.get_menaced_king_positions((2, 2), 3, 3) == result
+
+
+class TestRook:
+    def test_menaced_rook_positions__left_top_corner(self):
+        result = [(1, 0), (2, 0), (0, 1), (0, 2)]
+        assert pieces.get_menaced_rook_positions((0, 0), 3, 3) == result
+
+    def test_menaced_rook_positions__center(self):
+        result = [(2, 1), (0, 1), (1, 2), (1, 0)]
+        assert pieces.get_menaced_rook_positions((1, 1), 3, 3) == result
+
+    def test_menaced_rook_positions__right_bottom_corner(self):
+        result = [(1, 2), (0, 2), (2, 1), (2, 0)]
+        assert pieces.get_menaced_rook_positions((2, 2), 3, 3) == result
