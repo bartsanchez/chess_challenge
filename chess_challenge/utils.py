6cae0bde39bcaaadcc15f675f38d1bc14ea52bf6
Bartolomé Sánchez <bsanchezsalado@gmail.com>
2016-04-19 12:23:25 +0200

    Add function to generate all pieces combinations

--- chess_challenge/utils.py
+++ chess_challenge/utils.py
@@ -1,6 +1,13 @@
+import itertools
+
 PIECES = ['K', 'R']
 
 
 def are_valid_pieces(pieces):
     """Return True if pieces as argument are valid or False otherwise."""
     return all([p in PIECES for p in pieces])
+
+
+def get_pieces_combinations(pieces):
+    """Return all permutations without repetitions for a set of pieces."""
+    return set(list(itertools.permutations(pieces)))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,3 +7,16 @@ class TestPieces:
 
     def test_are_valid_pieces__false(self):
         assert utils.are_valid_pieces(['K', 'A', 'R']) is False
+
+
+class TestPiecesCombinations:
+    def test_pieces_combinations__K(self):
+        assert utils.get_pieces_combinations(['K']) == set([('K',)])
+
+    def test_pieces_combinations__KK(self):
+        result = set([('K', 'K')])
+        assert utils.get_pieces_combinations(['K', 'K']) == result
+
+    def test_pieces_combinations__KKR(self):
+        result = set([('K', 'K', 'R'), ('K', 'R', 'K'), ('R', 'K', 'K')])
+        assert utils.get_pieces_combinations(['K', 'K', 'R']) == result
