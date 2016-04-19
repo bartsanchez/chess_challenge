3b56820b69c181121239cb16b830a7c9cd0b625b
Bartolomé Sánchez <bsanchezsalado@gmail.com>
2016-04-19 12:36:05 +0200

    Fix small issues to satisfy pylint

--- chess_challenge/main.py
+++ chess_challenge/main.py
@@ -2,14 +2,14 @@
 Main functions.
 """
 
-import utils
+from chess_challenge import utils
 
 
 def solve(rows, cols, pieces):
     """Find all unique and valid configurations for the pieces in the board."""
 
     if not utils.are_valid_pieces(pieces):
-        print('The pieces passed as argument are not valid.')
+        print 'The pieces passed as argument are not valid.'
         return -1
 
     return 0
