08366f030126f7e9a13cb1ca920cb3322db7c8e6
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2016-04-20 10:55:29 +0200

    Add bishop menaces detection function

--- chess_challenge/pieces.py
+++ chess_challenge/pieces.py
@@ -2,12 +2,15 @@
 Pieces functions.
 """
 
+from chess_challenge import board
+
 
 def get_menaced_positions(piece, position, rows, cols):
     """Determine the function to call for a given piece."""
     pieces_functions = {
         'K': get_menaced_king_positions,
         'R': get_menaced_rook_positions,
+        'B': get_menaced_bishop_positions,
     }
     return pieces_functions[piece](position, rows, cols)
 
@@ -70,5 +73,38 @@ def get_menaced_rook_positions(position, rows, cols):
     while j - 1 >= 0:
         j -= 1
         result.append((current_i, j))
+
+    return result
+
+
+def get_menaced_bishop_positions(position, rows, cols):
+    """Return all menaced positions by a bishop."""
+    result = []
+
+    positions = list(board.get_coordinates_from_board(rows, cols))
+
+    current_i, current_j = position
+    while (current_i - 1, current_j - 1) in positions:
+        current_i -= 1
+        current_j -= 1
+        result.append((current_i, current_j))
+
+    current_i, current_j = position
+    while (current_i + 1, current_j - 1) in positions:
+        current_i += 1
+        current_j -= 1
+        result.append((current_i, current_j))
+
+    current_i, current_j = position
+    while (current_i - 1, current_j + 1) in positions:
+        current_i -= 1
+        current_j += 1
+        result.append((current_i, current_j))
+
+    current_i, current_j = position
+    while (current_i + 1, current_j + 1) in positions:
+        current_i += 1
+        current_j += 1
+        result.append((current_i, current_j))
 
     return result
--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -14,6 +14,11 @@ class TestMenacedPositionsCalls:
         pieces.get_menaced_positions('R', (0, 0), 3, 3)
         rook_mock.assert_called_once_with((0, 0), 3, 3)
 
+    @mock.patch('chess_challenge.pieces.get_menaced_bishop_positions')
+    def test_calling_bishop(self, bishop_mock):
+        pieces.get_menaced_positions('B', (0, 0), 3, 3)
+        bishop_mock.assert_called_once_with((0, 0), 3, 3)
+
 
 class TestKing:
     def test_menaced_king_positions__left_top_corner(self):
@@ -42,3 +47,17 @@ class TestRook:
     def test_menaced_rook_positions__right_bottom_corner(self):
         result = [(1, 2), (0, 2), (2, 1), (2, 0)]
         assert pieces.get_menaced_rook_positions((2, 2), 3, 3) == result
+
+
+class TestBishop:
+    def test_menaced_bishop_positions__left_top_corner(self):
+        result = [(1, 1), (2, 2)]
+        assert pieces.get_menaced_bishop_positions((0, 0), 3, 3) == result
+
+    def test_menaced_bishop_positions__center(self):
+        result = [(0, 0), (2, 0), (0, 2), (2, 2)]
+        assert pieces.get_menaced_bishop_positions((1, 1), 3, 3) == result
+
+    def test_menaced_bishop_positions__right_bottom_corner(self):
+        result = [(1, 1), (0, 0)]
+        assert pieces.get_menaced_bishop_positions((2, 2), 3, 3) == result
